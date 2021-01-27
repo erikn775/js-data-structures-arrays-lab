0b7f55e2159fafe88e9918bda39cd930a6be7e02
erikn775 <erikn775@gmail.com>
2021-01-27 11:39:47 -0700

    commit tree 9b4bbb8e851951a3bdf0c20f75c5a952b6876acb

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your solution here!
+let drivers = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendDriver(name) {
+    name.push('Ralph')
+}
+
+destructivelyAppendDriver(drivers)
